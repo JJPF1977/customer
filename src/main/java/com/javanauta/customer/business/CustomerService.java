a0c4ff45cc1099d4bb4af687bceb863ecd6bab6a
Joao <joao.profilho77@gmail.com>
2025-12-29 13:55:24 -0300

    Updated project costumer

--- src/main/java/com/javanauta/customer/business/CustomerService.java
+++ src/main/java/com/javanauta/customer/business/CustomerService.java
@@ -3,21 +3,54 @@ package com.javanauta.customer.business;
 import com.javanauta.customer.business.converter.CustomerConverter;
 import com.javanauta.customer.business.dto.CustomerDTO;
 import com.javanauta.customer.infrastructure.entity.Customer;
+import com.javanauta.customer.infrastructure.exceptions.ConflictException;
+import com.javanauta.customer.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
+@Service
 
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final CustomerConverter customerConverter;
+    private final PasswordEncoder passwordEncoder;
 
-    public CustomerDTO customerSave(CustomerDTO customerDTO){
+    public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+        emailExists(customerDTO.getEmail());
+        customerDTO.setPassword(passwordEncoder.encode(customerDTO.getPassword()));
         Customer customer = customerConverter.toCustomer(customerDTO);
         customer = customerRepository.save(customer);
         return customerConverter.toCustomerDTO(customer);
+    }
+
+    public void emailExists(String email) {
+        try {
+
+            boolean exists = checkExistingEmail(email);
+            if (exists) {
+                throw new ConflictException("Email already registered" + email);
+
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("Email already registered", e.getCause());
+        }
+
+    }
+
+    public boolean checkExistingEmail(String email) {
+        return customerRepository.existsByEmail(email);
+    }
+
+    public Customer searchCustomerByEmail(String email) {
+        return customerRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email not found" + email));
+    }
+
+    public void deleteCustomerByEmail(String email) {
+        customerRepository.deleteByEmail(email);
     }
 }
--- src/main/java/com/javanauta/customer/controller/CustomerController.java
+++ src/main/java/com/javanauta/customer/controller/CustomerController.java
@@ -2,24 +2,49 @@ package com.javanauta.customer.controller;
 
 import com.javanauta.customer.business.CustomerService;
 import com.javanauta.customer.business.dto.CustomerDTO;
+import com.javanauta.customer.infrastructure.entity.Customer;
+import com.javanauta.customer.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
-@RestController("/customer")
+@RestController
+@RequestMapping("/customer")
 @RequiredArgsConstructor
 
 public class CustomerController {
 
     private final CustomerService customerService;
-    @PostMapping
-    public ResponseEntity<CustomerDTO> customerSave(@RequestBody CustomerDTO customerDTO){
-       return ResponseEntity.ok(customerService.customerSave(customerDTO));
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
+    @PostMapping
+    public ResponseEntity<CustomerDTO> saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return ResponseEntity.ok(customerService.saveCustomer(customerDTO));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody CustomerDTO customerDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(customerDTO.getEmail(),
+                        customerDTO.getPassword())
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Customer> searchCustomerByEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(customerService.searchCustomerByEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deleteCustomerByEmail(@PathVariable String email){
+        customerService.deleteCustomerByEmail(email);
+        return ResponseEntity.ok().build();
     }
 
 }
